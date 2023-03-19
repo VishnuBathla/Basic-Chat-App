408632e70d8ce3dc2d662ce9869e3565aaae1091
VishnuBathla <87614276+VishnuBathla@users.noreply.github.com>
2023-03-19 22:30:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ class Client:
     def receive(self):
         while self.running:
             try:
-                message=self.sock.recv(1024)
+                message=self.sock.recv(1024).decode('utf-8')
                 if message =='Name':
                     self.sock.send(self.name.encode('utf-8'))
                 else:
